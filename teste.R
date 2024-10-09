f83b26662cc3c8e8dd0d969a7518d744e9d1f418
Miqueias Teixeira <miqueias.teixeira@gmail.com>
2024-10-09 20:03:47 -0300

    aula 04

--- teste.R
+++ teste.R
@@ -3,3 +3,27 @@ require(tidyverse)
 
 dados <- readr::read_csv("dados/Mental Health Dataset.csv")
 head(dados)
+
+glimpse(dados)
+
+
+Poland <- subset(dados, Country == "Poland")
+
+Coluna <- dados [, c("Country", "Gender")]
+Coluna
+
+
+require(data.table)
+require(magrittr)
+car_crash <- fread("dados/Brazil Total highway crashes 2010 - 2023.csv.gz")
+head(car_crash)
+glimpse(car_crash)
+
+acidente <- car_crash[, c("onibus,", "moto")]
+acidente
+
+car_crash %>% 
+  select(starts_with("tr"))
+
+##Escrever o que aconteceu na aula.
+
